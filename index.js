05bae9e1b1df00d225220e8bd4de1d328b58ff3d
Artem <partem645@gmail.com>
2024-03-15 14:02:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mysql = require('mysql');
+const page = require('./routes/page')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('privet');
+})
+
+app.use('/poster', page)
+
+app.get('/about', (req,res) => {
+
+})
+app.get('/price', (req, res) => {
+
+})
+app.get('/contacts', (req, res) => {
+    
+})
+
+
+app.listen(3000)
---
+++ routes/page.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/allDay', (req, res) => {
+
+});
+router.get('/today', (req, res) => {
+    
+});
+router.get('/tomorrow', (req, res) => {
+    
+});
